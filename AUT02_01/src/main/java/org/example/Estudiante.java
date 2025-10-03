c2d902a47c2d7954477e0dac8a74383325c27011
2dawb <2dawb@informatica.edu@PC234004.informatica.edu>
2025-10-03 10:45:46 +0100

    Fin de Clase

--- AUT02_01/src/main/java/org/example/Estudiante.java
+++ AUT02_01/src/main/java/org/example/Estudiante.java
@@ -5,4 +5,27 @@ public class Estudiante{
     private int edad;
     private float media;
     private Boolean matricula;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setEdad(int edad){
+        if (edad > 0){
+            this.edad = edad;
+        }
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setMedia(float media){
+        if (media >= 0 && media <= 10){
+            this.media = media;
+        }
+    public float getMedia(){
+            return media;
+        }
+    }
 }
